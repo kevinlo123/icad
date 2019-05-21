47e4061bbc4764c94968863aadd2de09204b8011
Kevin <lopezkevin175@gmail.com>
2019-05-20 22:59:08 -0400

    finished up desktop header / navbar

--- src/app/partials/navbars/DesktopNavBar.jsx
+++ src/app/partials/navbars/DesktopNavBar.jsx
@@ -5,52 +5,56 @@ class DesktopNavBar extends Component {
       render() {
          return (
             <nav className={"desktop-header"}>
-               <li>
-                  <NavLink 
-                     exact to="/"
-                     activeClassName="selected"
-                     className={""}>
-                     Home
+               <div className={"list-items-container"}>
+                  <li className={"list-items-container__list-item"}>
+                     <NavLink 
+                        exact to="/"
+                        activeClassName="selected"
+                        className={"list-items-container__list-item--link"}>
+                        Home
+                     </NavLink>
+                  </li>
+                  <li className={"list-items-container__list-item"}>
+                     <NavLink 
+                        to="/vehicle_wraps" 
+                        activeClassName="selected" 
+                        className={"list-items-container__list-item--link"}>
+                        Vehicle Wraps
+                     </NavLink>
+                  </li>
+                  <li className={"list-items-container__list-item"}>
+                     <NavLink 
+                        to="/large_format_printing" 
+                        activeClassName="selected" 
+                        className={"list-items-container__list-item--link"}>
+                        Large Format Printing
+                     </NavLink>
+                  </li>
+                  <li className={"list-items-container__list-item"}>
+                     <NavLink 
+                        to="/graphic_design" 
+                        activeClassName="selected" 
+                        className={"list-items-container__list-item--link"}>
+                        Graphic Design
+                     </NavLink>
+                  </li>
+                  <li className={"list-items-container__list-item"}>
+                     <NavLink 
+                        to="/about" 
+                        activeClassName="selected" 
+                        className={"list-items-container__list-item--link"}>
+                        About Us
+                     </NavLink>
+                  </li>
+               </div>
+               <div className={"phone-quote-container"}>
+                  <a href="tel:781-397-8166" className={"phone-quote-container__phone"}>
+                     <i className="fas fa-phone"></i>
+                  </a>
+                  <NavLink to="/quote" className={"phone-quote-container__quote"}>
+                     Get a Free Quote
                   </NavLink>
-               </li>
-               <li>
-                  <NavLink 
-                     to="/vehicle_wraps" 
-                     activeClassName="selected" 
-                     className={""}>
-                     Vehicle Wraps
-                  </NavLink>
-               </li>
-               <li>
-                  <NavLink 
-                     to="/large_format_printing" 
-                     activeClassName="selected" 
-                     className={""}>
-                     Large Format Printing
-                  </NavLink>
-               </li>
-               <li>
-                  <NavLink 
-                     to="/graphic_design" 
-                     activeClassName="selected" 
-                     className={""}>
-                     Graphic Design
-                  </NavLink>
-               </li>
-               <li>
-                  <NavLink 
-                     to="/about" 
-                     activeClassName="selected" 
-                     className={""}>
-                     About Us
-                  </NavLink>
-               </li>
-               <a href="tel:781-397-8166" className={""}>
-                  <i className="fas fa-phone"></i>
-               </a>
-               <NavLink to="/quote" className={""}>
-                  Get a Free Quote
-               </NavLink>
+               </div>
             </nav>
          );
       }
