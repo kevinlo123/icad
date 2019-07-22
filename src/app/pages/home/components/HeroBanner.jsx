212e35e56a3f46261046dfe5052d9f7b1d45124d
Kevin <lopezkevin175@gmail.com>
2019-07-22 07:40:55 -0400

    made changes

--- src/app/pages/home/components/HeroBanner.jsx
+++ src/app/pages/home/components/HeroBanner.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import WrappingVideo from '../../../../videos/wrapping.mp4'
 
 class HeroBanner extends Component {
+   // constructor() {
+   //    super()
+   //    this.video = React.createRef();
+   // }
+
+   // componentDidMount() {
+   //    console.log(this.video)
+   // }
+
    render() {
       return (
-         <section>
-            <h1>Hero banner</h1>
+         <section className={"vidContain"}>
+            <div className="vid">           
+                  <video muted playsInline autoPlay ref={this.video} preload="preload" loop="loop">
+                     <source src={WrappingVideo} type="video/mp4"></source>
+                  </video> 
+            </div>
+            <h1 className={"content"}>VIDEOO1!!!!</h1>
          </section>
       );
    }
